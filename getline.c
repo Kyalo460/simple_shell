d5998d4facb4fb93cf3cfd2c76b4bfb5756d132a
kyalo460 <kyalo460@gmail.com>
2023-08-18 10:49:09 +0000

    A function that extracts a line from stdin

---
+++ getline.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int fetch(char *tokens[])
+{
+	char *line;
+	int read;
+	size_t bytes = 0, n = 0;
+
+	tokens[0] = NULL;
+
+	while (tokens[0] == NULL)
+	{
+		printf("testShell~$ ");
+		read = getline(&line, &bytes, stdin);
+		if (read == -1)
+		{
+			printf("getline error\n");
+			exit(EXIT_FAILURE);
+		}
+
+		tokens[n] = strtok(line, " \n");
+		while (tokens[n] != NULL)
+		{
+			n++;
+			tokens[n] = strtok(NULL, " \n");
+		}
+	}
+	return (0);
+}
