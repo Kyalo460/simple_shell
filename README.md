582d2250526447361c6a02e533a948e876b8f958
kyalo460 <kyalo460@gmail.com>
2023-08-23 08:39:31 +0000

    Added a readme file + made _strncmp.c betty compilant

---
+++ README.md
@@ -0,0 +1,2 @@
+In this project we made a simple shell that can take commands from the user
+and execute them
--- _strncmp.c
+++ _strncmp.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
+/**
+  *_strncmp - checks if two strings are the same
+  *@str1: first string
+  *@str2: second string
+  *@num: number of bytes to compare
+  *
+  *Return: returns 0 if strings are the same
+  */
 int _strncmp(char *str1, char *str2, int num)
 {
 	char *string1 = str1;
 	char *string2 = str2;
 	int i = 0, diff;
 
-	while (i < num )
+	while (i < num)
 	{
 		if (*string1 == '\0' && *string2 == '\0')
 			break;
 
 		diff = *string1++ - *string2++;
-	
+
 		if (diff != 0)
 			return (diff);
 		i++;
